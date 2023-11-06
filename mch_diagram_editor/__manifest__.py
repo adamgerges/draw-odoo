a30726f66657406f37a8ea0d09fe51f391d7743d
adamgerges <adamgerges@gmail.com>
2023-11-05 19:56:21 -0800

    [MIG] mch_diagram_editor: Migration to 16.0

--- mch_diagram_editor/__manifest__.py
+++ mch_diagram_editor/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Diagram Editor',
-    'version': '14.0.1.0.0',
+    'version': "16.0.1.0.0",
     'category': 'Extra Tools',
     'license': 'LGPL-3',
     'description': 'Diagram Editor',
